a62cc6a288c36d4d308484d353df199835d31465
Sumit Yewale <sumityewale1@gmail.com>
2025-07-22 19:45:17 +0530

    Second commit

--- src/main/java/com/publicissapient/football/FootballStandingsServiceApplication.java
+++ src/main/java/com/publicissapient/football/FootballStandingsServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FootballStandingsServiceApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello publicis sapients");
 		SpringApplication.run(FootballStandingsServiceApplication.class, args);
 	}
 
